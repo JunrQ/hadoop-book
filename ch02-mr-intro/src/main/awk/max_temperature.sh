0105078f0b32898af683f2c755e5f0755be57dbc
JunrQ <137556741@qq.com>
2020-07-23 14:03:56 +0800

    Update:

--- ch02-mr-intro/src/main/awk/max_temperature.sh
+++ ch02-mr-intro/src/main/awk/max_temperature.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-for year in all/*
+ncdcds=${GIT_DIRECTORY}/hadoop-book/input/ncdc
+for year in ${ncdcds}/all/*
 do
-  echo -ne `basename $year .gz`"\t"
-  gunzip -c $year | \
+  echo -ne `basename $year`"\t"
+  cat $year | \
     awk '{ temp = substr($0, 88, 5) + 0;
            q = substr($0, 93, 1);
            if (temp !=9999 && q ~ /[01459]/ && temp > max) max = temp }
